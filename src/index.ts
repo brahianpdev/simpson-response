e81d6f6769005238ab413a4ea452f135171d90d9
brahianpdev <zk.spywn@gmail.com>
2022-11-22 23:29:09 -0300

    feat(index): entry point

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+import { SimpsonResponse } from './messages/http.message';
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// Example of use
+app.get('/', (req: Request, res: Response) => {
+	res.send(SimpsonResponse.UNAUTHORIZED);
+});
