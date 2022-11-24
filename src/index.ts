e2e40b61b36bb66fbcf4f0df51b2feaf2e8b43b6
brahianpdev <zk.spywn@gmail.com>
2022-11-23 23:43:26 -0300

    fix(docs): fix usage example and update package version

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,3 @@
-import express, { Request, Response } from 'express';
 import { SimpsonResponse } from './messages/simpson-response.message';
 
-const app = express();
-const port = 3000;
-
-app.listen(port, () => {
-	console.log(`Example app listening at http://localhost:${port}`);
-});
-
-// Example of use
-app.get('/', (req: Request, res: Response) => {
-	res.send(SimpsonResponse.UNAUTHORIZED);
-});
+export default SimpsonResponse;
